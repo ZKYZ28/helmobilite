dd74005c50b9b9407d2a773b270d75de33c7cf79
q210208 <f.mahy@student.helmo.be>
2023-02-25 13:10:39 +0100

    -

--- app/src/main/java/com/example/projettupreferes/models/Choice.kt
+++ app/src/main/java/com/example/projettupreferes/models/Choice.kt
@@ -1,6 +1,7 @@
 package com.example.projettupreferes.models
 
 import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
@@ -9,5 +10,5 @@ import java.util.*
 data class Choice(
     @PrimaryKey @NonNull val idChoice: UUID = UUID.randomUUID(),
     val textChoice : String = "",
-    val isText: Boolean
+    val isText: Boolean,
 )
--- app/src/main/java/com/example/projettupreferes/models/Statistics.kt
+++ app/src/main/java/com/example/projettupreferes/models/Statistics.kt
@@ -1,11 +1,7 @@
 package com.example.projettupreferes.models
 
-import androidx.annotation.NonNull
-import androidx.room.PrimaryKey
-import java.util.*
 
 class Statistics(
-    @PrimaryKey @NonNull val idStatistics: UUID = UUID.randomUUID(),
     var gamesPlayed : Int,
     var nbrCategories : Int,
     var nbrSwipes : Int,
