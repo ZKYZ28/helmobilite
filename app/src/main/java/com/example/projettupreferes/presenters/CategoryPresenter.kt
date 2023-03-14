dae593888e00564d25e18347d1fc1d5a7e266fd3
Edwin De Vlegelaer <e.devlegelaer@student.helmo.be>
2023-03-14 16:28:56 +0100

    US 7 - Fix fragment

--- app/src/main/java/com/example/projettupreferes/presenters/CategoryPresenter.kt
+++ app/src/main/java/com/example/projettupreferes/presenters/CategoryPresenter.kt
@@ -85,14 +85,11 @@ class CategoryPresenter(
     }
 
     fun switchToSeePairs() {
-        gameManager.currentCategoryWithPaires.paires.size
-        Log.d("Taille de la liste des paires", gameManager.currentCategoryWithPaires.paires.size.toString())
-        if(gameManager.currentCategoryWithPaires.paires.isEmpty()){
-            Log.d("CategoryPresenter", "ici")
+        if (gameManager.currentCategoryWithPaires.paires.isEmpty()) {
             categoryFragment?.showErrorMessage("Vous n'avez aucune paire liée à cette catégorie")
-        }else{
-            mainPresenter.requestSwitchView(FragmentsName.SeePair)
+            return
         }
+        mainPresenter.requestSwitchView(FragmentsName.SeePair)
     }
 
     fun switchToAddPair() {
